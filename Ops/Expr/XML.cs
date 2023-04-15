db9c6c0ad61ad1cc16aa00749bc12c1b39fa7af4
priyarathna <priyarathna@metamation.com>
2023-04-15 10:10:06 +0530

    A18.Q2: Refinement on XElement

--- Ops/Expr/XML.cs
+++ Ops/Expr/XML.cs
@@ -1,33 +1,15 @@
 ﻿namespace PSI;
 
 public class ExprXML : Visitor<XElement> {
-   public override XElement Visit (NLiteral literal) {
-      XElement node = new ("Literal");
-      node.SetAttributeValue ("Value", literal.Value.Text);
-      node.SetAttributeValue ("Type", literal.Type);
-      return node;
-   }
+   public override XElement Visit (NLiteral literal) => 
+      new XElement ("Literal", new XAttribute ("Value", literal.Value.Text), new XAttribute ("Type", literal.Type));
 
-   public override XElement Visit (NIdentifier identifier) {
-      XElement node = new ("Ident");
-      node.SetAttributeValue ("Name", identifier.Name);
-      node.SetAttributeValue ("Type", identifier.Type);
-      return node;
-   }
+   public override XElement Visit (NIdentifier identifier) =>
+      new XElement ("Ident", new XAttribute ("Name", identifier.Name), new XAttribute ("Type", identifier.Type));
 
-   public override XElement Visit (NUnary unary) {
-      XElement node = new ("Unary");
-      node.SetAttributeValue ("Op", unary.Op.Kind);
-      node.SetAttributeValue ("Type", unary.Type);
-      node.Add (unary.Expr.Accept (this));
-      return node;
-   }
+   public override XElement Visit (NUnary unary) =>
+      new XElement ("Unary", new XAttribute ("Op", unary.Op.Kind), new XAttribute ("Type", unary.Type), new XElement (unary.Expr.Accept (this)));
 
-   public override XElement Visit (NBinary binary) {
-      XElement node = new ("Binary");
-      node.SetAttributeValue ("Op", binary.Op.Kind);
-      node.SetAttributeValue ("Type", binary.Type);
-      node.Add (binary.Left.Accept (this)); node.Add (binary.Right.Accept (this));
-      return node;
-   }
+   public override XElement Visit (NBinary binary) =>
+      new XElement ("Binary", new XAttribute ("Op", binary.Op.Kind), new XAttribute ("Type", binary.Type), new XElement (binary.Left.Accept (this)), new XElement (binary.Right.Accept (this)));
 }
